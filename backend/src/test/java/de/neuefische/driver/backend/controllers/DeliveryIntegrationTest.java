defdb723c6ba2f8d0644b042a07abbefb9a0637d
devToJu <msg.toju@outlook.de>
2023-03-31 13:03:27 +0200

    backend: add integration-test for addDelivery

--- backend/src/test/java/de/neuefische/driver/backend/controllers/DeliveryIntegrationTest.java
+++ backend/src/test/java/de/neuefische/driver/backend/controllers/DeliveryIntegrationTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,5 +28,18 @@ class DeliveryIntegrationTest {
                 """));
     }
 
-
+    @Test
+    @DirtiesContext
+    void addDelivery_shouldReturnAddedDelivery() throws Exception {
+        mockMvc.perform(post("/api/deliveries")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"title":  "test"}
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {"title":  "test"}
+                        """))
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
 }
