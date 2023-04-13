0aec34b77f62d251a211bb49193ba1c87a374611
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-04-13 15:03:37 +0200

    fix bug EnvironmentIntegrationTest

--- backend/src/test/java/de/neuefische/driver/backend/controllers/EnvironmentIntegrationTest.java
+++ backend/src/test/java/de/neuefische/driver/backend/controllers/EnvironmentIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -17,6 +18,7 @@ class EnvironmentIntegrationTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void getEnvironmentName_shouldReturnStringTest() throws Exception{
         mockMvc.perform(get("/api/info"))
                 .andExpect(status().isOk())
